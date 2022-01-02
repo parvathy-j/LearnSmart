a04d20c80278535fa13ea39d6ed1c80060bfe265
Parvathy Jothi <86248688+parvathy-j@users.noreply.github.com>
2022-01-02 19:42:23 +0530

    Update timer.js

--- src/assets/js/timer.js
+++ src/assets/js/timer.js
@@ -1,70 +1,65 @@
-const pomodoroTimer = document.querySelector('#clockdiv');
-const minutesbtn=document.querySelector('minutes')
-const secondsbtn=document.querySelector('seconds')
-const startButton = document.querySelector('#start');
-const pauseButton = document.querySelector('#pause');
+const startButton = document.querySelector('#start')
 const stopButton = document.querySelector('#stop');
-
-// START
-startButton.addEventListener('click', () => {
-  toggleClock();
-})
-
-// PAUSE
-pauseButton.addEventListener('click', () => {
-  toggleClock();
-})
-
-// STOP
-stopButton.addEventListener('click', () => {
-  toggleClock(true);
-})
-let isClockRunning = false;
-let workSessionDuration = 1500;
-let currentTimeLeftInSession = 1500;
-let breakSessionDuration = 300;
-const toggleClock = (reset) => {
-    if (reset) {
-      // STOP THE TIMER
-    } else {
-      if (isClockRunning === true) {
-        clearInterval(clockTimer);
-        // PAUSE THE TIMER
-        isClockRunning = false;
-      } else {
-        // START THE TIMER
-        isClockRunning = true;
-        clockTimer = setInterval(() => {
-            // decrease time left / increase time spent
-            currentTimeLeftInSession--;
-            displayCurrentTimeLeftInSession();
-        }, 1000)
+breakDuration=5
+workDuration=25
+seconds=60
+breakcount=0
+workMinutes=workDuration-1
+breakMinutes=breakDuration-1
+let timer
+let minutetimer
+let RemainingTime=()=>{
+  seconds=seconds-1
+  if(seconds===0){
+    workMinutes=workMinutes-1
+    if(workMinutes===-1){
+      if(breakcount%2===0){
+        workMinutes=breakMinutes
+        breakcount++
+      }
+      else{
+      workMinutes=breakMinutes
+      breakcount++
       }
     }
+    seconds=60
+
   }
-  const displayCurrentTimeLeftInSession = () => {
-    const secondsLeft = currentTimeLeftInSession;
-    let result = '';
-    const seconds = secondsLeft % 60;
-    const minutes = parseInt(secondsLeft / 60) % 60;
-    let hours = parseInt(secondsLeft / 3600);
-    // add leading zeroes if it's less than 10
-    function addLeadingZeroes(time) {
-      return time < 10 ? `0${time}` : time
-    }
-    if (hours > 0)
-    //  result += `${hours}:`
-    // result += `${addLeadingZeroes(minutes)}:${addLeadingZeroes(seconds)}`
-    // // pomodoroTimer.innerText = result.toString();
+}
+
   
-pomodoroTimer.innerHTML=` <div class="clock-wrapper" style="background: rgb(47, 52, 56)">
-<span class="minutes">${addLeadingZeroes(minutes)}</span>
-<div class="smalltext">
-  <p>Minutes</p>
-</div>
-</div>
-<div class="clock-wrapper" style="background: rgb(47, 52, 56)">
-<span class="seconds"> ${addLeadingZeroes(seconds)}</span>
-<div class="smalltext">
-  <p>Seconds</p>
-</div>`}
+
+ 
+// const clickbtn=function (){
+startButton.addEventListener('click',()=>{
+  timer=setInterval(function(){
+    seconds--
+     RemainingTime
+     document.querySelector('.seconds').innerHTML=`${seconds}`
+    if(seconds<=0)seconds=60}, 1000);
+    document.querySelector('.minutes').innerHTML=`${workMinutes}`
+    
+    minutetimer=setInterval(function(){
+      workMinutes--
+       RemainingTime
+       document.querySelector('.minutes').innerHTML=`${workMinutes}`
+      if(seconds<=0)seconds=60}, 60*1000);
+    
+      
+
+  console.log(workMinutes)
+  console.log(seconds)
+console.log('clicked')
+console.log(timer)
+// document.querySelector('.minutes').innerHTML=`${workMinutes}`
+// document.querySelector('.seconds').innerText=seconds
+
+})
+// clickbtn()
+stopButton.addEventListener('click',()=>{
+  clearInterval(timer)
+    document.querySelector('.seconds').innerHTML=`00`
+  document.querySelector('.minutes').innerHTML=`25 `
+  
+
+  })
